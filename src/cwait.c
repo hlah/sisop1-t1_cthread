382a40a3f4bd70f0ebfac0ba2bed6fd7f54aa98d
Arturgh0 <42970945+Arturgh0@users.noreply.github.com>
2018-09-07 21:31:22 -0300

    Update cwait.c

--- src/cwait.c
+++ src/cwait.c
@@ -1,16 +1,12 @@
 #include "../include/cthread_internal.h"
 
 int cwait(csem_t *sem) {
-    // cthread_spin_lock();
-
     sem->count--;
+    
     if (sem->count < 0) { // bloqueia processo
-        // cthread_spin_unlock();
         sem->count++;
         return cthread_sem_block(sem);
     }
 
-    // cthread_spin_unlock();
     return 0;
-
 }
