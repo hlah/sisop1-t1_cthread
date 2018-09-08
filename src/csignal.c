a97076305b27c461397ca2e53aca4c425e1f7947
Arturgh0 <42970945+Arturgh0@users.noreply.github.com>
2018-09-07 21:30:54 -0300

    Update csignal.c

--- src/csignal.c
+++ src/csignal.c
@@ -1,5 +1,19 @@
 #include "../include/cthread_internal.h"
 
 int csignal(csem_t *sem) {
-	return -1;
+    int i;
+    PFILA2 fifoPrio;
+
+    sem->count++;
+    FirstFila2(sem->fila);
+
+    for (i = CTHREAD_NUM_PRIORITY_LEVELS - 1; i >= 0; i--){ // verifica se há uma fila não vazia
+        if (FirstFila2(fifoPrio = GetAtIteratorFila2(sem->fila)) != 0){
+            cthread_sem_wakeup(sem, fifoPrio, i);
+            return 0;
+        }
+        NextFila2(sem->fila);
+    }
+
+    return 0;
 }
