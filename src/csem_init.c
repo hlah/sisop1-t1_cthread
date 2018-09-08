0d65c1e2d8fb9211718f84d9cc64a169e013cde5
Arturgh0 <42970945+Arturgh0@users.noreply.github.com>
2018-09-07 21:29:35 -0300

    Update csem_init.c

--- src/csem_init.c
+++ src/csem_init.c
@@ -1,5 +1,17 @@
 #include "../include/cthread_internal.h"
 
 int csem_init(csem_t *sem, int count) {
-	return -1;
+    if (count > 0){
+        int i;
+        PFILA2 fifoPrio[CTHREAD_NUM_PRIORITY_LEVELS];
+
+        CreateFila2(sem -> fila);
+        for(i = 0; i < CTHREAD_NUM_PRIORITY_LEVELS; i++) // inicializa filas
+            fifoPrio[i] = CreateFila2(fifoPrio[i]);
+		for(i = 0; i < CTHREAD_NUM_PRIORITY_LEVELS; i++) // cria uma fila de bloqueados por prioridade
+			AppendFila2(sem -> fila, (void*) fifoPrio[i]);
+        sem -> count = count;
+        return 0;
+    }
+	return -1; // erro count <= 0
 }
