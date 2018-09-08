213457b859c6f420e728696e1905856e51816d04
Arturgh0 <42970945+Arturgh0@users.noreply.github.com>
2018-09-07 21:50:17 -0300

    Update cthread_sem_wakeup.c

--- src/cthread_sem_wakeup.c
+++ src/cthread_sem_wakeup.c
@@ -5,6 +5,7 @@ void cthread_sem_wakeup(csem_t* sem, PFILA2 fifoPrio, int priority){
     sem->count--;
     GetAtIteratorFila2(fifoPrio)->state = CTHREAD_STATE_APT;
     AppendFila2(&cthread_priority_fifos[priority], (void*) GetAtIteratorFila2(fifoPrio));
+    DeleteAtIteratorFila2(fifoPrio);
 
     if (priority > cthread_executing_thread->prio) // checa prioridade do processo bloqueado
         cthread_schedule(cthread_executing_thread, 0);
