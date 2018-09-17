4ebc4b5d30221b67773d404ff046e505f2ba4bfe
Arturgh0 <42970945+Arturgh0@users.noreply.github.com>
2018-09-17 19:35:44 -0300

    Update csem_init.c

--- src/csem_init.c
+++ src/csem_init.c
@@ -6,12 +6,12 @@ int csem_init(csem_t *sem, int count) {
         PFILA2 fifoPrio[CTHREAD_NUM_PRIORITY_LEVELS];
 
         CreateFila2(sem -> fila);
-        for(i = 0; i < CTHREAD_NUM_PRIORITY_LEVELS; i++) // inicializa filas
-            fifoPrio[i] = CreateFila2(fifoPrio[i]);
-		for(i = 0; i < CTHREAD_NUM_PRIORITY_LEVELS; i++) // cria uma fila de bloqueados por prioridade
-			AppendFila2(sem -> fila, (void*) fifoPrio[i]);
+        for(i = 0; i < CTHREAD_NUM_PRIORITY_LEVELS; i++) // inicializa uma fila de bloqueados por prioridade
+            CreateFila2(fifoPrio[i]);
+	for(i = 0; i < CTHREAD_NUM_PRIORITY_LEVELS; i++) // adiciona filas de prioridade a fila principal
+	    AppendFila2(sem -> fila, (void*) fifoPrio[i]);
         sem -> count = count;
         return 0;
     }
-	return -1; // erro count <= 0
+    return -1; // erro count <= 0
 }
