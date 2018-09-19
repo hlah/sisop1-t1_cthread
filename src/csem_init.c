83fe0d2e668b59b0a3316092975d8f6819caccc5
Arturgh0 <42970945+Arturgh0@users.noreply.github.com>
2018-09-18 22:34:42 -0300

    Added: debug print

--- src/csem_init.c
+++ src/csem_init.c
@@ -4,13 +4,15 @@ int csem_init(csem_t *sem, int count) {
     if (count > 0){
         int i;
         PFILA2 fifoPrio[CTHREAD_NUM_PRIORITY_LEVELS];
-
+	
+	DEBUG_PRINT("Creating new semaphore...\n");
         CreateFila2(sem -> fila);
         for(i = 0; i < CTHREAD_NUM_PRIORITY_LEVELS; i++) // inicializa uma fila de bloqueados por prioridade
             CreateFila2(fifoPrio[i]);
 	for(i = 0; i < CTHREAD_NUM_PRIORITY_LEVELS; i++) // adiciona filas de prioridade a fila principal
 	    AppendFila2(sem -> fila, (void*) fifoPrio[i]);
         sem -> count = count;
+	DEBUG_PRINT("Semaphore created!\n");
         return 0;
     }
     return -1; // erro count <= 0
