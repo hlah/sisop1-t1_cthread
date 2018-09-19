2a0664a541b353af8bc3437d01f50891d9c4571c
Arturgh0 <42970945+Arturgh0@users.noreply.github.com>
2018-09-18 22:45:52 -0300

    Added: debug print

--- src/cthread_sem_wakeup.c
+++ src/cthread_sem_wakeup.c
@@ -1,5 +1,7 @@
 #include "../include/cthread_internal.h"
 
+#include <stdio.h>
+
 int cthread_sem_wakeup(csem_t* sem, PFILA2 fifoPrio){
 
     TCB_t* unblocked_thread = GetAtIteratorFila2(fifoPrio);
@@ -8,6 +10,7 @@ int cthread_sem_wakeup(csem_t* sem, PFILA2 fifoPrio){
     unblocked_thread->state = CTHREAD_STATE_APTO;
     AppendFila2(&cthread_priority_fifos[unblocked_thread->prio], (void*) unblocked_thread);
     DeleteAtIteratorFila2(fifoPrio);
+    DEBUG_PRINT("Thread %d unblocked (resource available)\n", unblocked_thread->tid);
 
     if (unblocked_thread->prio > cthread_executing_thread->prio) // checa prioridade do processo bloqueado
         return cthread_schedule(cthread_executing_thread, 0);
