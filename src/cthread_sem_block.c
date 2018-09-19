21836c68105e71393a16688b7d047888ae11f291
Arturgh0 <42970945+Arturgh0@users.noreply.github.com>
2018-09-18 22:44:52 -0300

    Added: debug print

--- src/cthread_sem_block.c
+++ src/cthread_sem_block.c
@@ -1,5 +1,7 @@
 #include "../include/cthread_internal.h"
 
+#include <stdio.h>
+
 int cthread_sem_block(csem_t* sem){
     int prio = cthread_executing_thread->prio;
     FirstFila2(sem->fila);
@@ -11,5 +13,6 @@ int cthread_sem_block(csem_t* sem){
 
     cthread_executing_thread->state = CTHREAD_STATE_BLOCK;
     AppendFila2(GetAtIteratorFila2(sem->fila) , (void*) cthread_executing_thread); // coloca thread na lista de bloqueados
+    DEBUG_PRINT("Thread %d blocked (tried to access unavailable resource)\n", cthread_executing_thread->tid);
     return cthread_schedule(cthread_executing_thread, 1); // escalona proximo thread
  }
