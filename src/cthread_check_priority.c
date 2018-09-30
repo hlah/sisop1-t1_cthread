a00cf8a8439276be2bdd35056ff0357a1a9175ed
Mateus Davi Simon <hlah_@hotmail.com>
2018-09-30 12:44:13 -0300

    Fixed: cthread_check_priority

--- src/cthread_check_priority.c
+++ src/cthread_check_priority.c
@@ -3,7 +3,7 @@
 int cthread_check_priority(int prio) {
 	int i;
 	for(i=prio-1; i>=0; i--) {
-		if( FirstFila2(&cthread_priority_fifos[i]) != 0 ) {
+		if( FirstFila2(&cthread_priority_fifos[i]) == 0 ) {
 			return 1;
 		}
 	}
--- testes/testa_setprio.c
+++ testes/testa_setprio.c
@@ -7,15 +7,17 @@ void* mythread_low(void*);
 
 int main() {
 
-	int thread1 = ccreate(mythread_low, (void*)1, 0);
-	int thread2 = ccreate(mythread, (void*)2, 0);
-	int thread3 = ccreate(mythread, (void*)3, 0);
+	int thread1 = ccreate(mythread, (void*)1, 2);
+	int thread2 = ccreate(mythread_low, (void*)2, 2);
+	int thread3 = ccreate(mythread, (void*)3, 2);
 
 	cyield();
 
 	printf("main: i'm back!!!\n");
 
 	cjoin(thread1);
+	cjoin(thread2);
+	cjoin(thread3);
 
 	return 0;
 }
@@ -23,6 +25,10 @@ int main() {
 
 void* mythread(void* arg) {
 	int i=0;
+	printf("entering thread %d\n", (int)arg);
+
+	cyield();
+	csetprio(0, 0);
 
 	for( i=0; i<5; i++ ) {
 		printf("mytrhead %d: hello world (%d)\n", (int)arg, i+1);
@@ -34,8 +40,9 @@ void* mythread(void* arg) {
 
 void* mythread_low(void* arg) {
 	int i=0;
+	printf("entering thread %d\n", (int)arg);
 
-	csetprio(0, 2);
+	csetprio(0, 1);
 
 	for( i=0; i<5; i++ ) {
 		printf("mytrhead %d: hello world (%d)\n", (int)arg, i+1);
