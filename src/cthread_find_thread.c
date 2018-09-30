fafe4e677fd3a0ba124b7efa6b631d19a6b7f962
Mateus Davi Simon <hlah_@hotmail.com>
2018-09-30 20:33:20 -0300

    Fixed: variable declared in for loop

--- src/cthread_find_thread.c
+++ src/cthread_find_thread.c
@@ -4,13 +4,14 @@ TCB_t* cthread_find_thread(int tid) {
 	// procura nas threads criadas
 	TCB_t* next_thread = NULL;
 	int found = 0;
+	int i;
 	FirstFila2(&cthread_created_fifo);
 	while( !found && (next_thread = (TCB_t*)GetAtIteratorFila2(&cthread_created_fifo)) != NULL ) {
 		found = next_thread->tid == tid;
 		NextFila2(&cthread_created_fifo);
 	}
 	// procura threads aptos
-	for( int i=0; i<CTHREAD_NUM_PRIORITY_LEVELS; i++ ) {
+	for( i=0; i<CTHREAD_NUM_PRIORITY_LEVELS; i++ ) {
 		FirstFila2(&cthread_priority_fifos[i]);
 		while( !found && (next_thread = (TCB_t*)GetAtIteratorFila2(&cthread_priority_fifos[i])) != NULL ) {
 			found = next_thread->tid == tid;
